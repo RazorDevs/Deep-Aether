0dc2a9656db72d1ee5d67e9b97707be3146863bf
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2022-06-11 18:07:56 +0200

    Fixed Aerglow Fish Rotation
    Fixed Aerglow Fish Rotation and tried to add AI

--- src/main/java/teamrazor/deepaether/entity/AetherFishEntity.java
+++ src/main/java/teamrazor/deepaether/entity/AetherFishEntity.java
@@ -2,10 +2,14 @@
 package teamrazor.deepaether.entity;
 
 
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.goal.FollowFlockLeaderGoal;
 import net.minecraft.world.entity.animal.AbstractFish;
+import net.minecraft.world.entity.animal.AbstractSchoolingFish;
 import net.minecraft.world.entity.animal.Cod;
+import net.minecraft.world.level.ServerLevelAccessor;
 import teamrazor.deepaether.init.DeepAetherModEntities;
 
 import net.minecraftforge.registries.ForgeRegistries;
@@ -40,11 +44,17 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.core.BlockPos;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 @Mod.EventBusSubscriber
 public class AetherFishEntity extends Cod {
 	private static final Set<ResourceLocation> SPAWN_BIOMES = Set.of(new ResourceLocation("warm_ocean"));
+	@Nullable
+	private AetherFishEntity leader;
+	private int schoolSize = 1;
 
 	@SubscribeEvent
 	public static void addLivingEntityToBiomes(BiomeLoadingEvent event) {
@@ -170,6 +180,99 @@ public class AetherFishEntity extends Cod {
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
 		builder = builder.add(ForgeMod.SWIM_SPEED.get(), 0.3);
 		return builder;
+	}
+
+	public int getMaxSpawnClusterSize() {
+		return this.getMaxSchoolSize();
+	}
+
+	public int getMaxSchoolSize() {
+		return super.getMaxSpawnClusterSize();
+	}
+
+	protected boolean canRandomSwim() {
+		return !this.isFollower();
+	}
+
+	public boolean isFollower() {
+		return this.leader != null && this.leader.isAlive();
+	}
+
+	public AbstractSchoolingFish startFollowing(AetherFishEntity p_27526_) {
+		this.leader = p_27526_;
+		p_27526_.addFollow();
+		return p_27526_;
+	}
+
+	public void stopFollowing() {
+		this.leader.removeFollow();
+		this.leader = null;
+	}
+
+	private void addFollow() {
+		++this.schoolSize;
+	}
+
+	private void removeFollow() {
+		--this.schoolSize;
+	}
+
+	public boolean canBeFollowed() {
+		return this.hasFollowers() && this.schoolSize < this.getMaxSchoolSize();
+	}
+
+	public void tick() {
+		super.tick();
+		if (this.hasFollowers() && this.level.random.nextInt(200) == 1) {
+			List<? extends AbstractFish> list = this.level.getEntitiesOfClass(this.getClass(), this.getBoundingBox().inflate(8.0D, 8.0D, 8.0D));
+			if (list.size() <= 1) {
+				this.schoolSize = 1;
+			}
+		}
+
+	}
+
+	public boolean hasFollowers() {
+		return this.schoolSize > 1;
+	}
+
+	public boolean inRangeOfLeader() {
+		return this.distanceToSqr(this.leader) <= 121.0D;
+	}
+
+	public void pathToLeader() {
+		if (this.isFollower()) {
+			this.getNavigation().moveTo(this.leader, 1.0D);
+		}
+
+	}
+
+	public void addFollowers(Stream<? extends AbstractSchoolingFish> p_27534_) {
+		p_27534_.limit((long)(this.getMaxSchoolSize() - this.schoolSize)).filter((p_27538_) -> {
+			return p_27538_ != this;
+		}).forEach((p_27536_) -> {
+			p_27536_.startFollowing(this);
+		});
+	}
+
+	@Nullable
+	public SpawnGroupData finalizeSpawn(ServerLevelAccessor p_27528_, DifficultyInstance p_27529_, MobSpawnType p_27530_, @Nullable SpawnGroupData p_27531_, @Nullable CompoundTag p_27532_) {
+		super.finalizeSpawn(p_27528_, p_27529_, p_27530_, p_27531_, p_27532_);
+		if (p_27531_ == null) {
+			p_27531_ = new AbstractSchoolingFish.SchoolSpawnGroupData(this);
+		} else {
+			this.startFollowing(((AbstractSchoolingFish.SchoolSpawnGroupData)p_27531_).leader);
+		}
+
+		return p_27531_;
+	}
+
+	public static class SchoolSpawnGroupData implements SpawnGroupData {
+		public final AbstractSchoolingFish leader;
+
+		public SchoolSpawnGroupData(AbstractSchoolingFish p_27553_) {
+			this.leader = p_27553_;
+		}
 	}
 }
 
