23be00cf214e7b2fad6422fe04309f7716dedbbd
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2022-06-25 13:00:31 +0200

    Fixed Block Ticking Behaviour

--- src/main/java/teamrazor/deepaether/block/AerglowGrassBlockBlock.java
+++ src/main/java/teamrazor/deepaether/block/AerglowGrassBlockBlock.java
@@ -1,34 +1,29 @@
 
 package teamrazor.deepaether.block;
 
-import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.material.Material;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.item.ItemStack;
+import com.gildedgames.aether.common.block.natural.AetherGrassBlock;
+import com.gildedgames.aether.common.registry.AetherBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SnowLayerBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.lighting.LayerLightEngine;
+import net.minecraft.world.level.material.Material;
 
-import java.util.List;
-import java.util.Collections;
+import java.util.Random;
 
-public class AerglowGrassBlockBlock extends Block {
+public class AerglowGrassBlockBlock extends AetherGrassBlock {
+
 	public AerglowGrassBlockBlock() {
-		super(BlockBehaviour.Properties.of(Material.GRASS).sound(SoundType.GRAVEL).strength(1f, 10f));
-	}
-
-	@Override
-	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 15;
-	}
-
-	@Override
-	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
-		if (!dropsOriginal.isEmpty())
-			return dropsOriginal;
-		return Collections.singletonList(new ItemStack(this, 1));
+		super(BlockBehaviour.Properties.of(Material.GRASS)
+				.sound(SoundType.GRAVEL)
+				.strength(1f, 10f)
+				.randomTicks());
 	}
 }
