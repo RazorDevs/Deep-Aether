4ffe0acf8bef40ed1ef067aac6b2d23f7a13b252
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2022-07-06 16:01:31 +0200

    Major fix

--- models/mojmap-1.18.x/Modelcustom_model.java
+++ models/mojmap-1.18.x/Modelcustom_model.java
@@ -1,19 +1,34 @@
+package teamrazor.deepaether.client.model;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.LayerDefinition;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.EntityModel;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.blaze3d.vertex.PoseStack;
+
 // Made with Blockbench 4.2.4
 // Exported for Minecraft version 1.17 - 1.18 with Mojang mappings
 // Paste this class into your mod and generate all required imports
-
 public class Modelcustom_model<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in
 	// the entity renderer and passed into this model's constructor
-	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
-			new ResourceLocation("modid", "custom_model"), "main");
-	private final ModelPart body;
-	private final ModelPart head;
-	private final ModelPart nose;
-	private final ModelPart fin_left;
-	private final ModelPart fin_right;
-	private final ModelPart fin_back;
-	private final ModelPart tail;
+	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation("deep_aether", "modelcustom_model"), "main");
+	public final ModelPart body;
+	public final ModelPart head;
+	public final ModelPart nose;
+	public final ModelPart fin_left;
+	public final ModelPart fin_right;
+	public final ModelPart fin_back;
+	public final ModelPart tail;
 
 	public Modelcustom_model(ModelPart root) {
 		this.body = root.getChild("body");
@@ -28,51 +43,36 @@ public class Modelcustom_model<T extends Entity> extends EntityModel<T> {
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
-
-		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(
-				-1.0F, -2.0F, 0.0F, 2.0F, 4.0F, 9.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 22.0F, 0.0F));
-
+		PartDefinition body = partdefinition.addOrReplaceChild("body",
+				CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -2.0F, 0.0F, 2.0F, 4.0F, 9.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 22.0F, 0.0F));
 		PartDefinition right_fin = body.addOrReplaceChild("right_fin",
-				CubeListBuilder.create().texOffs(24, 1).addBox(-2.0F, 0.0F, -1.0F, 2.0F, 0.0F, 2.0F,
-						new CubeDeformation(0.0F)),
+				CubeListBuilder.create().texOffs(24, 1).addBox(-2.0F, 0.0F, -1.0F, 2.0F, 0.0F, 2.0F, new CubeDeformation(0.0F)),
 				PartPose.offsetAndRotation(-1.0F, 1.0F, 0.0F, 0.0F, 0.0F, -0.7854F));
-
-		PartDefinition left_fin = body
-				.addOrReplaceChild("left_fin",
-						CubeListBuilder.create().texOffs(24, 4).addBox(0.0F, 0.0F, -1.0F, 2.0F, 0.0F, 2.0F,
-								new CubeDeformation(0.0F)),
-						PartPose.offsetAndRotation(1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.7854F));
-
-		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(13, 0).addBox(
-				-1.0F, -2.0F, -3.0F, 2.0F, 4.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 22.0F, 0.0F));
-
-		PartDefinition nose = partdefinition.addOrReplaceChild("nose", CubeListBuilder.create().texOffs(0, 0).addBox(
-				-1.0F, -2.0F, -1.0F, 2.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 22.0F, -3.0F));
-
-		PartDefinition fin_left = partdefinition.addOrReplaceChild("fin_left", CubeListBuilder.create(),
-				PartPose.offset(0.0F, 24.0F, 0.0F));
-
-		PartDefinition fin_right = partdefinition.addOrReplaceChild("fin_right", CubeListBuilder.create(),
-				PartPose.offset(0.0F, 24.0F, 0.0F));
-
-		PartDefinition fin_back = partdefinition.addOrReplaceChild("fin_back", CubeListBuilder.create().texOffs(20, -6)
-				.addBox(0.0F, -2.0F, -1.0F, 0.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)),
+		PartDefinition left_fin = body.addOrReplaceChild("left_fin",
+				CubeListBuilder.create().texOffs(24, 4).addBox(0.0F, 0.0F, -1.0F, 2.0F, 0.0F, 2.0F, new CubeDeformation(0.0F)),
+				PartPose.offsetAndRotation(1.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.7854F));
+		PartDefinition head = partdefinition.addOrReplaceChild("head",
+				CubeListBuilder.create().texOffs(13, 0).addBox(-1.0F, -2.0F, -3.0F, 2.0F, 4.0F, 3.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 22.0F, 0.0F));
+		PartDefinition nose = partdefinition.addOrReplaceChild("nose",
+				CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, -2.0F, -1.0F, 2.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)),
+				PartPose.offset(0.0F, 22.0F, -3.0F));
+		PartDefinition fin_left = partdefinition.addOrReplaceChild("fin_left", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition fin_right = partdefinition.addOrReplaceChild("fin_right", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition fin_back = partdefinition.addOrReplaceChild("fin_back",
+				CubeListBuilder.create().texOffs(20, -6).addBox(0.0F, -2.0F, -1.0F, 0.0F, 2.0F, 6.0F, new CubeDeformation(0.0F)),
 				PartPose.offset(0.0F, 20.0F, 0.0F));
-
-		PartDefinition tail = partdefinition.addOrReplaceChild("tail", CubeListBuilder.create(),
-				PartPose.offset(0.0F, 22.0F, 7.0F));
-
+		PartDefinition tail = partdefinition.addOrReplaceChild("tail", CubeListBuilder.create(), PartPose.offset(0.0F, 22.0F, 7.0F));
 		PartDefinition tail_sub_0 = tail.addOrReplaceChild("tail_sub_0",
-				CubeListBuilder.create().texOffs(22, 3).mirror()
-						.addBox(0.0F, -4.0F, 9.0F, 0.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false),
+				CubeListBuilder.create().texOffs(22, 3).mirror().addBox(0.0F, -4.0F, 9.0F, 0.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false),
 				PartPose.offset(0.0F, 2.0F, -7.0F));
-
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
 
 	@Override
-	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay,
-			float red, float green, float blue, float alpha) {
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green,
+							   float blue, float alpha) {
 		body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		head.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 		nose.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
@@ -82,8 +82,7 @@ public class Modelcustom_model<T extends Entity> extends EntityModel<T> {
 		tail.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 
-	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw,
-			float headPitch) {
+	public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		this.head.xRot = ageInTicks;
 		this.nose.xRot = ageInTicks;
 		this.fin_left.xRot = ageInTicks;
