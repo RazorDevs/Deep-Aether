1e4a19846448bcfe9eceb7f1d3d48b3fbb3d0478
TunefulTurnip <99967759+TunefulTurnip@users.noreply.github.com>
2022-11-30 10:17:02 +0100

    Minor Fix, Updated Aether Version

--- src/main/java/teamrazor/deepaether/item/gear/cloudium/CloudiumSwordItem.java
+++ src/main/java/teamrazor/deepaether/item/gear/cloudium/CloudiumSwordItem.java
@@ -1,6 +1,8 @@
 package teamrazor.deepaether.item.gear.cloudium;
 
 import com.gildedgames.aether.item.combat.abilities.weapon.GravititeWeapon;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 
@@ -8,5 +10,10 @@ public class CloudiumSwordItem extends SwordItem implements GravititeWeapon {
 
     public CloudiumSwordItem(Tier p_43269_, int p_43270_, float p_43271_, Properties p_43272_) {
         super(p_43269_, p_43270_, p_43271_, p_43272_);
+    }
+
+    public boolean hurtEnemy(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        this.launchEntity(target, attacker);
+        return super.hurtEnemy(stack, target, attacker);
     }
 }
