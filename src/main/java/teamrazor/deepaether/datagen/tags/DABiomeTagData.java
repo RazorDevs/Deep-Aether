f7388062a4492bf6c83cb5fd60e22c47cce49224
darksonic300 <fabiross096@gmail.com>
2023-04-13 12:30:53 +0200

    Feature: Lake Oasis generation full implementation

--- src/main/java/teamrazor/deepaether/datagen/tags/DABiomeTagData.java
+++ src/main/java/teamrazor/deepaether/datagen/tags/DABiomeTagData.java
@@ -1,7 +1,6 @@
 package teamrazor.deepaether.datagen.tags;
 
 import com.gildedgames.aether.AetherTags;
-import com.gildedgames.aether.data.resources.registries.AetherBiomes;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.BiomeTagsProvider;
@@ -28,10 +27,6 @@ public class DABiomeTagData extends BiomeTagsProvider {
         );
 
         this.tag(DATags.Biomes.IS_NOT_SWAMP).add(
-                AetherBiomes.SKYROOT_FOREST,
-                AetherBiomes.SKYROOT_GROVE,
-                AetherBiomes.SKYROOT_MEADOW,
-                AetherBiomes.SKYROOT_WOODLAND,
                 DABiomes.AERLAVENDER_FIELDS,
                 DABiomes.AERGLOW_FOREST,
                 DABiomes.MYSTIC_AERGLOW_FOREST,
