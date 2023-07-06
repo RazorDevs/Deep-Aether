85d5a9af21ea80b0bef5bb3f8260842a5598c127
darksonic300 <fabiross096@gmail.com>
2023-07-06 14:24:24 +0200

    Fix: Sterling Aercloud now propagates light.

--- src/main/java/teamrazor/deepaether/block/SterlingAercloudBlock.java
+++ src/main/java/teamrazor/deepaether/block/SterlingAercloudBlock.java
@@ -1,31 +1,15 @@
 package teamrazor.deepaether.block;
 
 import com.aetherteam.aether.block.AetherBlocks;
-import com.aetherteam.aether.block.FrictionCapped;
-import com.aetherteam.aether.block.natural.AercloudBlock;
-import com.aetherteam.aether.block.natural.QuicksoilBlock;
-import com.aetherteam.aether.effect.AetherEffects;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.HalfTransparentBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import teamrazor.deepaether.init.DABlocks;
 
 public class SterlingAercloudBlock extends HalfTransparentBlock {
 
@@ -44,5 +28,10 @@ public class SterlingAercloudBlock extends HalfTransparentBlock {
         level.setBlockAndUpdate(pos, AetherBlocks.COLD_AERCLOUD.get().defaultBlockState());
 
         super.entityInside(state, level, pos, entity);
+    }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState p_49928_, BlockGetter p_49929_, BlockPos p_49930_) {
+        return true;
     }
 }
