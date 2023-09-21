dae266f935d80b5c8a8842ecd8c327393d1ae363
TunefulTurnip <tore.ekerhult@gmail.com>
2023-09-21 10:32:36 +0200

    Made Yagroot Swamp slightly larger

--- src/main/java/teamrazor/deepaether/world/biomes/DARegion.java
+++ src/main/java/teamrazor/deepaether/world/biomes/DARegion.java
@@ -61,14 +61,12 @@ public class DARegion extends Region {
         this.addBiome(mapper, new Climate.ParameterPoint(tempDefault4, Climate.Parameter.span(0.1F, 0.4F), fullRange, fullRange, fullRange, fullRange, 0),
                 AetherBiomes.SKYROOT_WOODLAND);
         // Row 5
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(-1.0F, 0.0F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(-1.0F, 0.5F), fullRange, fullRange, fullRange, fullRange, 0),
                 DABiomes.YAGROOT_SWAMP);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(0.0F, 0.3F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(0.5F, 0.7F), fullRange, fullRange, fullRange, fullRange, 0),
                 DABiomes.AERGLOW_FOREST);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(0.3F, 0.5F), fullRange, fullRange, fullRange, fullRange, 0),
+        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(0.7F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
                 DABiomes.BLUE_AERGLOW_FOREST);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(0.5F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
-                AetherBiomes.SKYROOT_FOREST);
 
         this.addBiome(mapper, new Climate.ParameterPoint(tempGold, Climate.Parameter.span(-1.0F, -0.6F), fullRange, fullRange, fullRange, fullRange, 0),
                 DABiomes.GOLDEN_HEIGHTS);
