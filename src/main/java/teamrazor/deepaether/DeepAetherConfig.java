687c5c10cd09d597ace0ec492e38d16825003ccd
TunefulTurnip <tore.ekerhult@gmail.com>
2023-10-06 07:47:58 +0200

    Added Client config

--- src/main/java/teamrazor/deepaether/DeepAetherConfig.java
+++ src/main/java/teamrazor/deepaether/DeepAetherConfig.java
@@ -1,5 +1,7 @@
 package teamrazor.deepaether;
 
+import net.minecraftforge.common.ForgeConfig;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 import org.apache.commons.lang3.tuple.Pair;
@@ -9,7 +11,7 @@ public class DeepAetherConfig {
     public static class Common {
         public final ConfigValue<Boolean> skyjade_enchant;
         public final ConfigValue<Integer> deep_aether_biome_weight;
-        public final ConfigValue<Boolean> always_enable_hallowen_slider;
+        public final ConfigValue<Boolean> always_enable_halloween_content;
 
         public Common(ForgeConfigSpec.Builder builder) {
             builder.push("Gameplay");
@@ -27,10 +29,29 @@ public class DeepAetherConfig {
             builder.pop();
 
             builder.push("Gameplay");
-            always_enable_hallowen_slider = builder
-                    .comment("Always Enables the hallowen Slider")
-                    .translation("config.deep_aether.common.gameplay.always_enable_hallowen_slider")
-                    .define("Always Enable Hallowen Slider", false);
+            always_enable_halloween_content = builder
+                    .comment("Always Enable halloween content, excluding the halloween slider")
+                    .translation("config.deep_aether.common.gameplay.always_enable_halloween_content")
+                    .define("Always Enable Halloween Content", false);
+            builder.pop();
+        }
+    }
+    public static class Client {
+        public final ConfigValue<Boolean> always_enable_halloween_slider;
+        public final ConfigValue<Boolean> never_enable_halloween_slider;
+        public Client(ForgeConfigSpec.Builder builder) {
+            builder.push("Rendering");
+            always_enable_halloween_slider = builder
+                    .comment("Always Enables the halloween Slider")
+                    .translation("config.deep_aether.common.gameplay.always_enable_halloween_slider")
+                    .define("Always Enable Halloween Slider", false);
+            builder.pop();
+
+            builder.push("Rendering");
+            never_enable_halloween_slider = builder
+                    .comment("Never Enables the halloween Slider, not even during halloween")
+                    .translation("config.deep_aether.common.gameplay.never_enable_halloween_slider")
+                    .define("Never Enable Halloween Slider", false);
             builder.pop();
         }
     }
@@ -39,9 +60,16 @@ public class DeepAetherConfig {
     public static final ForgeConfigSpec COMMON_SPEC;
     public static final Common COMMON;
 
+    public static final ForgeConfigSpec CLIENT_SPEC;
+    public static final Client CLIENT;
+
     static {
         final Pair<Common, ForgeConfigSpec> commonSpecPair = new ForgeConfigSpec.Builder().configure(Common::new);
         COMMON_SPEC = commonSpecPair.getRight();
         COMMON = commonSpecPair.getLeft();
+
+        final Pair<Client, ForgeConfigSpec> clientSpecPair = new ForgeConfigSpec.Builder().configure(Client::new);
+        CLIENT_SPEC = clientSpecPair.getRight();
+        CLIENT = clientSpecPair.getLeft();
     }
 }
