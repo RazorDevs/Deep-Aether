b3eac80a84020ee558c2b855e7645bd0dabcc0d6
TunefulTurnip <tore.ekerhult@gmail.com>
2023-10-08 14:45:28 +0200

    Slightly nerfed stratus boosted jump

--- src/main/java/teamrazor/deepaether/item/gear/stratus/StratusArmor.java
+++ src/main/java/teamrazor/deepaether/item/gear/stratus/StratusArmor.java
@@ -15,7 +15,7 @@ public interface StratusArmor {
             if (entity instanceof Player player) {
                 AetherPlayer.get(player).ifPresent(aetherPlayer -> {
                     if (aetherPlayer.isGravititeJumpActive()) {
-                        player.push(0.0, 1.5 * (float) EquipmentUtil.handleStratusRingBoost(player), 0.0);
+                        player.push(0.0, 1.3 * (float) EquipmentUtil.handleStratusRingBoost(player), 0.0);
                         if (player instanceof ServerPlayer serverPlayer) {
                             serverPlayer.connection.send(new ClientboundSetEntityMotionPacket(serverPlayer));
                         }
