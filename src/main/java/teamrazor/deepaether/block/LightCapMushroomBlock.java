503256b363b45312d910bd2e3ace0f045ab47014
darksonic300 <fabiross096@gmail.com>
2023-10-14 13:33:57 +0200

    Feature: Lightcap Mushrooms

---
+++ src/main/java/teamrazor/deepaether/block/LightCapMushroomBlock.java
@@ -0,0 +1,33 @@
+package teamrazor.deepaether.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.MushroomBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import teamrazor.deepaether.init.DABlocks;
+
+import java.util.function.Supplier;
+
+public class LightCapMushroomBlock extends MushroomBlock {
+
+
+    public LightCapMushroomBlock(Properties properties, Supplier<Holder<? extends ConfiguredFeature<?, ?>>> supplier) {
+        super(properties, supplier);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        if(state.is(DABlocks.ROSEROOT_LOG.get())) {
+            return true;
+        }
+        return super.canSurvive(state, level, pos);
+    }
+
+    @Override
+    public boolean isValidBonemealTarget(BlockGetter blockGetter, BlockPos pos, BlockState blockState, boolean b) {
+        return false;
+    }
+}
