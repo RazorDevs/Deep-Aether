8bd99d67dce158f7a0500408cf71449091d3c886
TunefulTurnip <tore.ekerhult@gmail.com>
2023-10-22 10:12:32 +0200

    Lightcap mushroom can be placed in any light level

--- src/main/java/teamrazor/deepaether/block/LightCapMushroomBlock.java
+++ src/main/java/teamrazor/deepaether/block/LightCapMushroomBlock.java
@@ -1,32 +1,31 @@
 package teamrazor.deepaether.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.world.level.BlockGetter;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.MushroomBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import teamrazor.deepaether.init.DABlocks;
-
-import java.util.function.Supplier;
 
 public class LightCapMushroomBlock extends MushroomBlock {
-
-    public LightCapMushroomBlock(Properties properties, Supplier<Holder<? extends ConfiguredFeature<?, ?>>> supplier) {
-        super(properties, supplier);
+    public LightCapMushroomBlock(Properties p_256027_, ResourceKey<ConfiguredFeature<?, ?>> p_256049_) {
+        super(p_256027_, p_256049_);
     }
 
     @Override
     public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
-        if(state.is(DABlocks.ROSEROOT_LOG.get())) {
+        BlockPos blockpos = pos.below();
+        BlockState blockstate = level.getBlockState(blockpos);
+        if (blockstate.is(BlockTags.MUSHROOM_GROW_BLOCK)) {
             return true;
+        } else {
+            return blockstate.canSustainPlant(level, blockpos, net.minecraft.core.Direction.UP, this);
         }
-        return super.canSurvive(state, level, pos);
     }
 
     @Override
-    public boolean isValidBonemealTarget(BlockGetter blockGetter, BlockPos blockPos, BlockState blockState, boolean b) {
+    public boolean isValidBonemealTarget(LevelReader p_255904_, BlockPos p_54871_, BlockState p_54872_, boolean p_54873_) {
         return false;
     }
 }
