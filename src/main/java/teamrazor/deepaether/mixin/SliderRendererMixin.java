721242109fe60f7d288785fba8faefda1008e908
TunefulTurnip <tore.ekerhult@gmail.com>
2023-10-27 18:58:04 +0200

    Implemented some textures

---
+++ src/main/java/teamrazor/deepaether/mixin/SliderRendererMixin.java
@@ -0,0 +1,44 @@
+package teamrazor.deepaether.mixin;
+
+import com.aetherteam.aether.client.renderer.entity.SliderRenderer;
+import com.aetherteam.aether.client.renderer.entity.model.SliderModel;
+import com.aetherteam.aether.entity.monster.dungeon.boss.Slider;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import teamrazor.deepaether.DeepAetherConfig;
+import teamrazor.deepaether.DeepAetherMod;
+
+@Mixin(value = SliderRenderer.class, remap = false)
+public abstract class SliderRendererMixin extends MobRenderer<Slider, SliderModel> {
+
+    @Unique
+    private static final ResourceLocation HALLOWEEN_SLIDER_ASLEEP_TEXTURE = new ResourceLocation(DeepAetherMod.MODID, "textures/entity/slider/halloween_slider_asleep.png");
+    @Unique
+    private static final ResourceLocation HALLOWEEN_SLIDER_ASLEEP_CRITICAL_TEXTURE = new ResourceLocation(DeepAetherMod.MODID, "textures/entity/slider/halloween_slider_asleep_critical.png");
+    @Unique
+    private static final ResourceLocation HALLOWEEN_SLIDER_AWAKE_TEXTURE = new ResourceLocation(DeepAetherMod.MODID, "textures/entity/slider/halloween_slider_awake.png");
+    @Unique
+    private static final ResourceLocation HALLOWEEN_SLIDER_AWAKE_CRITICAL_TEXTURE = new ResourceLocation(DeepAetherMod.MODID, "textures/entity/slider/halloween_slider_awake_critical.png");
+
+
+    public SliderRendererMixin(EntityRendererProvider.Context p_174304_, SliderModel p_174305_, float p_174306_) {
+        super(p_174304_, p_174305_, p_174306_);
+    }
+
+    @Inject(at = @At("HEAD"), method = "getTextureLocation(Lcom/aetherteam/aether/entity/monster/dungeon/boss/Slider;)Lnet/minecraft/resources/ResourceLocation;", cancellable = true)
+    public void getTextureLocation(Slider slider, CallbackInfoReturnable<ResourceLocation> cir) {
+        if(((DeepAetherMod.IS_HALLOWEEN || DeepAetherConfig.CLIENT.always_enable_halloween_slider.get()) && (!DeepAetherConfig.CLIENT.never_enable_halloween_slider.get()))) {
+            if (!slider.isAwake()) {
+                cir.setReturnValue(!slider.isCritical() ? HALLOWEEN_SLIDER_ASLEEP_TEXTURE : HALLOWEEN_SLIDER_ASLEEP_CRITICAL_TEXTURE);
+            } else {
+                cir.setReturnValue(!slider.isCritical() ? HALLOWEEN_SLIDER_AWAKE_TEXTURE : HALLOWEEN_SLIDER_AWAKE_CRITICAL_TEXTURE);
+            }
+        }
+    }
+}
