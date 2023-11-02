6b4b4bc66761586121d9634f1d9f3cb53481994b
darksonic300 <fabiross096@gmail.com>
2023-11-02 17:03:18 +0100

    Backport: pt.18

---
+++ src/main/java/teamrazor/deepaether/event/DAGeneralEvents.java
@@ -0,0 +1,47 @@
+package teamrazor.deepaether.event;
+
+import com.aetherteam.aether.entity.AetherEntityTypes;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.ShieldBlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import teamrazor.deepaether.DeepAetherMod;
+import teamrazor.deepaether.init.DAItems;
+
+@Mod.EventBusSubscriber(modid = DeepAetherMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class DAGeneralEvents {
+
+    @SubscribeEvent
+    public static void onLivingEntityDeath(LivingDeathEvent event) {
+        LivingEntity entity = event.getEntity();
+        if (entity.getType() == AetherEntityTypes.SLIDER.get() && DeepAetherMod.IS_HALLOWEEN_CONTENT_ENABLED) {
+            entity.spawnAtLocation(new ItemStack(DAItems.SPOOKY_RING.get(), 1));
+        }
+    }
+
+    @SubscribeEvent
+    public static void onShieldBlock(ShieldBlockEvent event)
+    {
+        var blocker = event.getEntity();
+        DamageSource source = event.getDamageSource();
+        if(ModList.get().isLoaded(DeepAetherMod.LOST_AETHER_CONTENT)) {
+            if (blocker.getUseItem().is(TagKey.create(Registries.ITEM, new ResourceLocation(DeepAetherMod.LOST_AETHER_CONTENT, "aether_shields")))) {
+                blocker.level().playSound(null, blocker.blockPosition(), SoundEvents.ZOMBIE_ATTACK_IRON_DOOR, blocker.getSoundSource(), 0.4F, 0.8F + blocker.level().random.nextFloat() * 0.4F);
+
+                if (blocker.getUseItem().getItem() == DAItems.STRATUS_SHIELD.get() && source.getDirectEntity() instanceof LivingEntity attacker) {
+                    attacker.knockback(1.5F, blocker.getX() - attacker.getX(), blocker.getZ() - attacker.getZ());
+                    attacker.setPos(attacker.getX(), attacker.getY() + 1D, attacker.getZ());
+                    attacker.hasImpulse = true;
+                }
+            }
+        }
+    }
+}
