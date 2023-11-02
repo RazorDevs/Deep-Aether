d05203690d5a787ea25ccc5fc1eed8b75c4fed5f
darksonic300 <fabiross096@gmail.com>
2023-11-02 16:52:59 +0100

    Backport: pt.13

---
+++ src/main/java/teamrazor/deepaether/block/YagrootVineBlock.java
@@ -0,0 +1,46 @@
+package teamrazor.deepaether.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.VineBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+
+public class YagrootVineBlock extends VineBlock {
+    public static final BooleanProperty BOTTOM = BlockStateProperties.BOTTOM;
+
+    public YagrootVineBlock(Properties properties) {
+        super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(BOTTOM, true).setValue(UP, false).setValue(NORTH, false).setValue(EAST, false).setValue(SOUTH, false).setValue(WEST, false));
+    }
+
+    public BlockState updateShape(BlockState state, Direction direction, BlockState p_57877_, LevelAccessor level, BlockPos pos, BlockPos p_57880_) {
+        boolean isBottom = !level.getBlockState(pos.below()).is(this);
+
+
+        if (direction == Direction.DOWN) {
+            return state.setValue(YagrootVineBlock.BOTTOM, isBottom);
+        } else {
+            BlockState blockstate = this.getUpdatedState(state, level, pos);
+            return !this.hasFaces(blockstate) ? Blocks.AIR.defaultBlockState() : blockstate.setValue(YagrootVineBlock.BOTTOM, isBottom);
+        }
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> state) {
+        state.add(BOTTOM);
+        super.createBlockStateDefinition(state);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        boolean isBottom = !context.getLevel().getBlockState(context.getClickedPos().below()).is(this);
+        return super.getStateForPlacement(context).setValue(BOTTOM, isBottom);
+    }
+}
