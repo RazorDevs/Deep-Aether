5fede1b20076bbcb78e9146e80bf6d6c8781780a
darksonic300 <fabiross096@gmail.com>
2023-11-26 11:11:56 +0100

    slider flawless boss drop

--- src/main/java/teamrazor/deepaether/client/keys/DeepAetherKeys.java
+++ src/main/java/teamrazor/deepaether/client/keys/DeepAetherKeys.java
@@ -11,9 +11,11 @@ import teamrazor.deepaether.DeepAetherMod;
 @Mod.EventBusSubscriber(modid = DeepAetherMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DeepAetherKeys {
     public final static KeyMapping STRATUS_DASH_ABILITY = new KeyMapping("key.deep_aether.stratus_dash_ability.desc", GLFW.GLFW_KEY_R, "key.aether.category");
+    public final static KeyMapping SLIDER_EYE_SLAM_ABILITY = new KeyMapping("key.deep_aether.slider_eye_ability", GLFW.GLFW_KEY_LEFT_ALT, "key.aether.category");
 
     @SubscribeEvent
     public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
         event.register(STRATUS_DASH_ABILITY);
+        event.register(SLIDER_EYE_SLAM_ABILITY);
     }
 }
