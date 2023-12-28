3868658822ec182dee32c5d29d12765c8e7906c5
TunefulTurnip <tore.ekerhult@gmail.com>
2023-12-28 16:50:44 +0100

    Update DARegion.java

--- src/main/java/teamrazor/deepaether/world/biomes/DARegion.java
+++ src/main/java/teamrazor/deepaether/world/biomes/DARegion.java
@@ -40,40 +40,40 @@ public class DARegion extends Region {
         Climate.Parameter tempDefault4 = Climate.Parameter.span(0.4F, 0.8F);
         Climate.Parameter tempDefault5 = Climate.Parameter.span(0.8F, 1.0F);
 
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault1, fullRange, fullRange, fullRange, fullRange, fullRange, 0),
-                AetherBiomes.SKYROOT_WOODLAND);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault1, fullRange, fullRange, fullRange, fullRange, fullRange, 0),
+                    AetherBiomes.SKYROOT_WOODLAND);
         // Row 2
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(-1.0F, -0.6F), fullRange, fullRange, fullRange, fullRange, 0),
-                BlueAerglowForest);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(-0.6F, 0.0F), fullRange, fullRange, fullRange, fullRange, 0),
-                AerglowForest);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(0.0F, 0.7F), fullRange, fullRange, fullRange, fullRange, 0),
-                MysticAerglowForest);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(0.7F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
-                AerlavenderFields);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(-1.0F, -0.6F), fullRange, fullRange, fullRange, fullRange, 0),
+                    BlueAerglowForest);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(-0.6F, 0.0F), fullRange, fullRange, fullRange, fullRange, 0),
+                    AerglowForest);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(0.0F, 0.7F), fullRange, fullRange, fullRange, fullRange, 0),
+                    MysticAerglowForest);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault2, Climate.Parameter.span(0.7F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
+                    AerlavenderFields);
         // Row 3
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault3, Climate.Parameter.span(-1.0F, -0.5F), fullRange, fullRange, fullRange, fullRange, 0),
-                AetherBiomes.SKYROOT_FOREST);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault3, Climate.Parameter.span(-0.5F, 0.5F), fullRange, fullRange, fullRange, fullRange, 0),
-                AerlavenderFields);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault3, Climate.Parameter.span(0.5F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
-                AetherBiomes.SKYROOT_GROVE);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault3, Climate.Parameter.span(-1.0F, -0.5F), fullRange, fullRange, fullRange, fullRange, 0),
+                    AetherBiomes.SKYROOT_FOREST);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault3, Climate.Parameter.span(-0.5F, 0.5F), fullRange, fullRange, fullRange, fullRange, 0),
+                    AerlavenderFields);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault3, Climate.Parameter.span(0.5F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
+                    AetherBiomes.SKYROOT_GROVE);
 
         // Row 4
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault4, Climate.Parameter.span(-1.0F, -0.3F), fullRange, fullRange, fullRange, fullRange, 0),
-                AerglowForest);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault4, Climate.Parameter.span(-0.3F, 0.2F), fullRange, fullRange, fullRange, fullRange, 0),
-                GoldenGrove);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault4, Climate.Parameter.span(0.2F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
-                GoldenHeights);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault4, Climate.Parameter.span(-1.0F, -0.3F), fullRange, fullRange, fullRange, fullRange, 0),
+                    AerglowForest);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault4, Climate.Parameter.span(-0.3F, 0.2F), fullRange, fullRange, fullRange, fullRange, 0),
+                    GoldenGrove);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault4, Climate.Parameter.span(0.2F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
+                    GoldenHeights);
         // Row 5
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(-1.0F, 0.7F), fullRange, fullRange, fullRange, fullRange, 0),
-                BlueAerglowForest);
-        this.addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(0.7F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
-                AetherBiomes.SKYROOT_FOREST);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(-1.0F, 0.7F), fullRange, fullRange, fullRange, fullRange, 0),
+                    BlueAerglowForest);
+        addBiome(mapper, new Climate.ParameterPoint(tempDefault5, Climate.Parameter.span(0.7F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
+                    AetherBiomes.SKYROOT_FOREST);
 
-        this.addBiome(mapper, new Climate.ParameterPoint(tempGold, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
-                YagrootSwamp);
+        addBiome(mapper, new Climate.ParameterPoint(tempGold, Climate.Parameter.span(-1.0F, 1.0F), fullRange, fullRange, fullRange, fullRange, 0),
+                    YagrootSwamp);
 
     }
 }
