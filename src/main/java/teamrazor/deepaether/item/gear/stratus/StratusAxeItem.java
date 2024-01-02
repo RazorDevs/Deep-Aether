728a81b3b47709f43f6fbacb77ad56a87c3143e4
darksonic300 <fabiross096@gmail.com>
2024-01-02 16:30:19 +0100

    Enhance: Some texture enhancements

--- src/main/java/teamrazor/deepaether/item/gear/stratus/StratusAxeItem.java
+++ src/main/java/teamrazor/deepaether/item/gear/stratus/StratusAxeItem.java
@@ -8,8 +8,8 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
 public class StratusAxeItem extends AxeItem implements GravititeTool {
-    public StratusAxeItem(Tier p_40521_, float p_40522_, float p_40523_, Properties p_40524_) {
-        super(p_40521_, p_40522_, p_40523_, p_40524_);
+    public StratusAxeItem(Tier tier, float v, float v1, Properties properties) {
+        super(tier, v, v1, properties);
     }
     @Override
     public InteractionResult useOn(UseOnContext context) {
