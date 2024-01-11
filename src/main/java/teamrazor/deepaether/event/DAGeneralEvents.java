69f8c27c818b4c1683fd0a84bf4004a715abdf58
darksonic300 <fabiross096@gmail.com>
2024-01-11 22:14:40 +0100

    Fix: Removed MissingEntries class

--- src/main/java/teamrazor/deepaether/event/DAGeneralEvents.java
+++ src/main/java/teamrazor/deepaether/event/DAGeneralEvents.java
@@ -2,21 +2,18 @@ package teamrazor.deepaether.event;
 
 import com.aetherteam.aether.entity.AetherEntityTypes;
 import com.aetherteam.aether.entity.passive.Moa;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.stats.Stats;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.entity.living.*;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.living.MobEffectEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import teamrazor.deepaether.DeepAetherMod;
 import teamrazor.deepaether.entity.IFlawlessBossDrop;
@@ -53,24 +50,6 @@ public class DAGeneralEvents {
         LivingEntity entity = event.getEntity();
         if (entity.getType() == AetherEntityTypes.SLIDER.get() && DeepAetherMod.IsHalloweenContentEnabled()) {
             entity.spawnAtLocation(new ItemStack(DAItems.SPOOKY_RING.get(), 1));
-        }
-    }
-
-    @SubscribeEvent
-    public static void onShieldBlock(ShieldBlockEvent event)
-    {
-        var blocker = event.getEntity();
-        DamageSource source = event.getDamageSource();
-        if(ModList.get().isLoaded(DeepAetherMod.LOST_AETHER_CONTENT)) {
-            if (blocker.getUseItem().is(TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(DeepAetherMod.LOST_AETHER_CONTENT, "aether_shields")))) {
-                blocker.getLevel().playSound(null, blocker.blockPosition(), SoundEvents.ZOMBIE_ATTACK_IRON_DOOR, blocker.getSoundSource(), 0.4F, 0.8F + blocker.getLevel().random.nextFloat() * 0.4F);
-
-                if (blocker.getUseItem().getItem() == DAItems.STRATUS_SHIELD.get() && source.getDirectEntity() instanceof LivingEntity attacker) {
-                    attacker.knockback(1.5F, blocker.getX() - attacker.getX(), blocker.getZ() - attacker.getZ());
-                    attacker.setPos(attacker.getX(), attacker.getY() + 1D, attacker.getZ());
-                    attacker.hasImpulse = true;
-                }
-            }
         }
     }
 
