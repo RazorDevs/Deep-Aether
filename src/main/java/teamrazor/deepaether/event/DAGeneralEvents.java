9259e1c9e44b2df1eed0bb7d3663523baf308975
TunefulTurnip <tore.ekerhult@gmail.com>
2024-01-28 13:16:29 +0100

    Removed a unused import

--- src/main/java/teamrazor/deepaether/event/DAGeneralEvents.java
+++ src/main/java/teamrazor/deepaether/event/DAGeneralEvents.java
@@ -3,7 +3,6 @@ package teamrazor.deepaether.event;
 import com.aetherteam.aether.entity.AetherBossMob;
 import com.aetherteam.aether.entity.AetherEntityTypes;
 import com.aetherteam.aether.entity.passive.Moa;
-import com.aetherteam.aether.event.BossFightEvent;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
