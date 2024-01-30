dc582d3082f146eef5846b0d41a5792d2ce41538
TunefulTurnip <99967759+TunefulTurnip@users.noreply.github.com>
2024-01-30 12:17:30 +0100

    Improved flawless boss fight system

--- src/main/java/teamrazor/deepaether/event/DAServerEvents.java
+++ src/main/java/teamrazor/deepaether/event/DAServerEvents.java
@@ -1,26 +1,16 @@
 package teamrazor.deepaether.event;
 
+import com.aetherteam.aether.event.BossFightEvent;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import teamrazor.deepaether.DeepAetherMod;
+import teamrazor.deepaether.entity.IPlayerBossFight;
 
 @Mod.EventBusSubscriber(modid = DeepAetherMod.MODID,  value = Dist.DEDICATED_SERVER, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class DAServerEvents {
-    /*@SubscribeEvent
-    public static void squashRandomizer(BlockEvent.CropGrowEvent event) {
-        if (event.getState().is(DABlocks.ATTACHED_SQUASH_STEM.get())) {
-            if (event.getLevel().getBlockState(event.getPos().north()).is(DABlocks.BLUE_SQUASH.get())) {
-                if(new Random().nextBoolean()) event.getLevel().setBlock(event.getPos().north(), DABlocks.GREEN_SQUASH.get().defaultBlockState(), 1);
-            }
-            if (event.getLevel().getBlockState(event.getPos().south()).is(DABlocks.BLUE_SQUASH.get())) {
-                if(new Random().nextBoolean()) event.getLevel().setBlock(event.getPos().south(), DABlocks.GREEN_SQUASH.get().defaultBlockState(), 1);
-            }
-            if (event.getLevel().getBlockState(event.getPos().east()).is(DABlocks.BLUE_SQUASH.get())) {
-                if(new Random().nextBoolean()) event.getLevel().setBlock(event.getPos().east(), DABlocks.GREEN_SQUASH.get().defaultBlockState(), 1);
-            }
-            if (event.getLevel().getBlockState(event.getPos().west()).is(DABlocks.BLUE_SQUASH.get())) {
-                if(new Random().nextBoolean()) event.getLevel().setBlock(event.getPos().west(), DABlocks.GREEN_SQUASH.get().defaultBlockState(), 1);
-            }
-        }
-    }*/
+    @SubscribeEvent
+    public static void onDungeonPlayerAdded(BossFightEvent.AddPlayer event) {
+        ((IPlayerBossFight) event.getPlayer()).deep_Aether$setHasBeenHurt(false);
+    }
 }
--- src/main/java/teamrazor/deepaether/mixin/AerwhaleKingMixin.java
+++ src/main/java/teamrazor/deepaether/mixin/AerwhaleKingMixin.java
@@ -1,10 +1,8 @@
-package teamrazor.deepaether.mixin.flawless;
+package teamrazor.deepaether.mixin;
 
 import com.aetherteam.aether.entity.AetherBossMob;
-import com.aetherteam.aether.entity.NpcDialogue;
-import com.aetherteam.aether.entity.monster.dungeon.AbstractValkyrie;
-import com.aetherteam.aether.entity.monster.dungeon.boss.ValkyrieQueen;
-import com.aetherteam.aether.item.AetherItems;
+import com.aetherteam.aether.entity.monster.dungeon.boss.SunSpirit;
+import com.legacy.lost_aether.entity.AerwhaleKingEntity;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -12,26 +10,28 @@ import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.FlyingMob;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.entity.IEntityAdditionalSpawnData;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import teamrazor.deepaether.DeepAetherConfig;
-import teamrazor.deepaether.advancement.DAAdvancementTriggers;
 import teamrazor.deepaether.entity.IPlayerBossFight;
 import teamrazor.deepaether.init.DAItems;
 
 import javax.annotation.Nullable;
 
-@Mixin(value = ValkyrieQueen.class)
-public abstract class ValkyrieQueenMixin implements AetherBossMob<ValkyrieQueen> {
+@Pseudo
+@Mixin(value = AerwhaleKingEntity.class)
+public abstract class AerwhaleKingMixin implements AetherBossMob<AerwhaleKingEntity> {
+
     @Inject(at = @At("TAIL"), method = "onDungeonPlayerAdded", remap = false)
     private void onDungeonPlayerAdded(Player player, CallbackInfo ci) {
         if (player instanceof ServerPlayer serverPlayer) {
