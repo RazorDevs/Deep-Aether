3569490d676db1bf8b84e5d10509ce099f99dc40
TunefulTurnip <tunefulturnip@gmail.com>
2024-08-07 09:49:23 +0200

    Update loot modifiers

--- src/main/java/teamrazor/deepaether/datagen/loot/modifiers/DAGlobalLootModifiers.java
+++ src/main/java/teamrazor/deepaether/datagen/loot/modifiers/DAGlobalLootModifiers.java
@@ -1,7 +1,6 @@
 package teamrazor.deepaether.datagen.loot.modifiers;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -13,7 +12,4 @@ public class DAGlobalLootModifiers {
     public static DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIERS = DeferredRegister.create(NeoForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, DeepAether.MODID);
 
     public static DeferredHolder<Codec<? extends IGlobalLootModifier>, Codec<? extends IGlobalLootModifier>> AETHER_DUNGEON_LOOT_CODEC = LOOT_MODIFIERS.register("aether_dungeon_loot", DAAddDungeonLootModifier.CODEC);
-    public static DeferredHolder<Codec<? extends IGlobalLootModifier>, Codec<? extends IGlobalLootModifier>> AETHER_FORCE_DUNGEON_LOOT_CODEC = LOOT_MODIFIERS.register("aether_force_dungeon_loot", DAAddFlatDungeonLootModifier.CODEC);
-    public static DeferredHolder<Codec<? extends IGlobalLootModifier>, Codec<? extends IGlobalLootModifier>> AETHER_DUNGEON_LOOT_CODEC_NO_REPLACEMENTS = LOOT_MODIFIERS.register("aether_dungeon_loot_no_replacement", DAAddDungeonLootModifierNoReplacements.CODEC);
-
 }
--- src/main/java/teamrazor/deepaether/datagen/loot/modifiers/DALootDataProvider.java
+++ src/main/java/teamrazor/deepaether/datagen/loot/modifiers/DALootDataProvider.java
@@ -22,44 +22,47 @@ public class DALootDataProvider extends GlobalLootModifierProvider {
 
     @Override
     protected void start() {
-        add("silver_loot_modifiers", new DAAddDungeonLootModifierNoReplacements(
+        add("silver_loot_modifiers", new DAAddDungeonLootModifier(
                 new LootItemCondition[] { LootTableIdCondition.builder(AetherLoot.SILVER_DUNGEON).build() },
                 List.of(
                         WeightedEntry.wrap(new ItemStack(DAItems.SKYJADE.get(), 1), 90),
                         WeightedEntry.wrap(new ItemStack(DAItems.SKYJADE.get(), 2), 10)
                         ),
                 100,
-                0.8f
+                0.0f
         ));
 
-        add("silver_loot_reward_modifiers", new DAAddDungeonLootModifierNoReplacements(
+        add("silver_loot_reward_modifiers", new DAAddDungeonLootModifier(
                 new LootItemCondition[] { LootTableIdCondition.builder(AetherLoot.SILVER_DUNGEON_REWARD).build() },
                 List.of(
-                        WeightedEntry.wrap(new ItemStack(DAItems.SKYJADE.get(), 2), 90),
-                        WeightedEntry.wrap(new ItemStack(DAItems.STRATUS_INGOT.get(), 1), 5),
-                        WeightedEntry.wrap(new ItemStack(DABlocks.STERLING_AERCLOUD.get(), 1), 10),
+                        WeightedEntry.wrap(new ItemStack(DAItems.SKYJADE.get(), 2), 65),
+                        WeightedEntry.wrap(new ItemStack(DAItems.STRATUS_INGOT.get(), 1), 10),
+                        WeightedEntry.wrap(new ItemStack(DABlocks.STERLING_AERCLOUD.get(), 1), 15),
                         WeightedEntry.wrap(new ItemStack(DAItems.STRATUS_SMITHING_TEMPLATE.get(), 1), 25)
                 ),
-                130,
-                0.5f
+                100,
+                0.0f
         ));
 
-        add("gold_loot_modifiers", new DAAddDungeonLootModifierNoReplacements(
+        add("gold_loot_modifiers", new DAAddDungeonLootModifier(
                 new LootItemCondition[] { LootTableIdCondition.builder(AetherLoot.GOLD_DUNGEON_REWARD).build() },
                 List.of(
                         WeightedEntry.wrap(new ItemStack(DAItems.SKYJADE.get(), 2), 60),
                         WeightedEntry.wrap(new ItemStack(DAItems.SKYJADE.get(), 5), 20),
                         WeightedEntry.wrap(new ItemStack(DAItems.STRATUS_INGOT.get(), 1), 10),
-                        WeightedEntry.wrap(new ItemStack(DABlocks.STERLING_AERCLOUD.get(), 2), 5),
-                        WeightedEntry.wrap(new ItemStack(DABlocks.STERLING_AERCLOUD.get(), 1), 15)
+                        WeightedEntry.wrap(new ItemStack(DABlocks.STERLING_AERCLOUD.get(), 2), 7),
+                        WeightedEntry.wrap(new ItemStack(DABlocks.STERLING_AERCLOUD.get(), 1), 20)
                 ),
-                110,
-                0.55f
+                100,
+                0.25f
         ));
 
-        add("gold_loot_stratus_upgrade", new DAAddFlatDungeonLootModifier(
+        add("gold_loot_stratus_upgrade", new DAAddDungeonLootModifier(
                 new LootItemCondition[] { LootTableIdCondition.builder(AetherLoot.GOLD_DUNGEON_REWARD).build() },
-                    new ItemStack(DAItems.STRATUS_SMITHING_TEMPLATE.get(), 1),
+                List.of(
+                        WeightedEntry.wrap(new ItemStack(DAItems.STRATUS_SMITHING_TEMPLATE.get(), 1), 100)
+                ),
+                100,
                 0.0f
         ));
     }
