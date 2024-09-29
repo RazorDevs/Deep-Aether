8f4e8bbfc7f00d9e3a60de2d9d9dcd9b64dd2df9
TunefulTurnip <tunefulturnip@gmail.com>
2024-09-29 10:31:08 +0200

    Added some missing lang entries

--- src/main/java/teamrazor/deepaether/datagen/loot/DALoot.java
+++ src/main/java/teamrazor/deepaether/datagen/loot/DALoot.java
@@ -19,7 +19,7 @@ public class DALoot {
     public static final ResourceLocation BRASS_DUNGEON_DISC = register("chests/dungeon/brass/brass_dungeon_disc");
     public static final ResourceLocation BRASS_DUNGEON_REWARD = register("chests/dungeon/brass/brass_dungeon_reward");
     public static final ResourceLocation BRASS_DUNGEON_TREASURE = register("chests/dungeon/brass/brass_dungeon_treasure");
-    public static final ResourceLocation BRASS_DUNGEON_STORM_FORGED = register("chests/dungeon/brass/brass_dungeon_stormforfed");
+    public static final ResourceLocation BRASS_DUNGEON_STORM_FORGED = register("chests/dungeon/brass/brass_dungeon_stormforged");
     public static final ResourceLocation BRASS_DUNGEON_GUMMIES = register("chests/dungeon/brass/brass_dungeon_gummies");
     
     private static ResourceLocation register(String id) {
