7a3aa2a6e52523d6c8967cb8c79ee625c046e2f5
TunefulTurnip <tunefulturnip@gmail.com>
2025-01-25 13:46:57 +0100

    Fix Aerwhale Saddle renderer

--- src/main/java/teamrazor/deepaether/mixin/AerwhaleRendererMixin.java
+++ src/main/java/teamrazor/deepaether/mixin/AerwhaleRendererMixin.java
@@ -24,7 +24,6 @@ public abstract class AerwhaleRendererMixin extends MobRenderer<Aerwhale, Entity
     private void AerwhaleRenderer(EntityRendererProvider.Context context, CallbackInfo ci) {
         if(AetherConfig.CLIENT.legacy_models.get())
             this.addLayer(new ClassicAerwhaleModelLayer(this));
-        else if(AetherConfig.CLIENT.legacy_models.get())
-            this.addLayer(new AerwhaleModelLayer(this));
+        else this.addLayer(new AerwhaleModelLayer(this));
     }
 }
