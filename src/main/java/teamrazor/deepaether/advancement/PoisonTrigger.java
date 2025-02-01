2e7e49fff3e1f9f800e44c9827b663159c4a63fb
TunefulTurnip <tunefulturnip@gmail.com>
2025-02-01 11:04:54 +0100

    Optimize Imports x2

--- src/main/java/teamrazor/deepaether/advancement/PoisonTrigger.java
+++ src/main/java/teamrazor/deepaether/advancement/PoisonTrigger.java
@@ -1,9 +1,7 @@
 package teamrazor.deepaether.advancement;
 
 import com.google.gson.JsonObject;
-import net.minecraft.advancements.critereon.ContextAwarePredicate;
-import net.minecraft.advancements.critereon.ItemPredicate;
-import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
+import net.minecraft.advancements.critereon.*;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
