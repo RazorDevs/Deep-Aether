8d592f3328ec697be5446e43834ddac21a10a1f2
TunefulTurnip <tunefulturnip@gmail.com>
2025-02-14 11:43:39 +0100

    Fix: Add Combiner Recipe

--- src/main/java/teamrazor/deepaether/datagen/DARecipeData.java
+++ src/main/java/teamrazor/deepaether/datagen/DARecipeData.java
@@ -664,6 +664,17 @@ public class DARecipeData extends AetherRecipeProvider {
         this.stonecuttingRecipe(consumer, RecipeCategory.BUILDING_BLOCKS, DABlocks.LIGHT_NIMBUS_PILLAR.get(), DABlocks.NIMBUS_PILLAR.get());
         this.stonecuttingRecipe(consumer, RecipeCategory.BUILDING_BLOCKS, DABlocks.NIMBUS_PILLAR.get(), DABlocks.LIGHT_NIMBUS_PILLAR.get());
 
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, DABlocks.COMBINER.get(), 1)
+                .define('O', DAItems.SKYJADE.get())
+                .define('W', AetherBlocks.SKYROOT_PLANKS.get())
+                .define('H', AetherBlocks.HOLYSTONE.get())
+                .define('A', AetherItems.AMBROSIUM_SHARD.get())
+                .pattern("WHW")
+                .pattern("OAO")
+                .pattern("HOH")
+                .unlockedBy(getHasName(DAItems.SKYJADE.get()), has(DAItems.SKYJADE.get()))
+                .save(consumer, name("combiner"));
+
         //Misc
         ShapedRecipeBuilder.shaped(RecipeCategory.COMBAT, DAItems.AFTERBURNER.get(), 1)
                 .define('O', DAItems.SUN_CORE.get())
