15f05196c8086eaf00141082f6a32f25ce912aae
darksonic300 <98578683+darksonic300@users.noreply.github.com>
2025-02-19 16:56:42 +0100

    fix: Closes #239.

--- src/main/java/teamrazor/deepaether/block/EmptyLog.java
+++ src/main/java/teamrazor/deepaether/block/EmptyLog.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -18,7 +19,7 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nonnull;
 
-public class EmptyLog extends DALogBlock {
+public class EmptyLog extends DALogBlock implements SimpleWaterloggedBlock {
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public EmptyLog(Properties properties) {
         super(properties);
