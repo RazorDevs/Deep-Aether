41ccc9ba95c15a3885a6442e3bea48f8c28a0445
AriaElidove <senthebun@gmail.com>
2025-10-14 05:47:46 +0200

    Merge Poison Datagen into Recipe-Tag-Fix. Also fixes some issue?

--- src/main/java/io/github/razordevs/deep_aether/datagen/tags/DAItemTagData.java
+++ src/main/java/io/github/razordevs/deep_aether/datagen/tags/DAItemTagData.java
@@ -12,6 +12,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.common.Tags;
